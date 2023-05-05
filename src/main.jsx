2e5084e005b29380cdee540527dcd18ecb1130dc
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-05-05 19:58:33 +0200

    [TASK] first commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client' 
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <h1>HR Manager</h1>
+    }
+  ]
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode> 
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
