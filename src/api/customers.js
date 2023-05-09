6b0db606c1fab8851bb8b30dc490fa5649911f13
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-05-09 18:27:45 +0200

    [TASK] create a function to call customer api

---
+++ src/api/customers.js
@@ -0,0 +1,24 @@
+
+export async function getCustomers() {
+    const response = await fetch(import.meta.env.VITE_API_URL)
+    const resquest = await response.json()
+    return resquest
+}
+
+
+export async function addCustomer(data) {
+    try {
+        const response = await fetch(import.meta.env.VITE_API_URL, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        await response.json()
+    }
+    catch(error) {
+        console.log(error)
+    }
+    
+}
